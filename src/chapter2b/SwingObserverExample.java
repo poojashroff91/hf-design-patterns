0ecf328a6d1f57da82811ac9ce0a70c7d67c9e31
Pooja Shroff <poshroff@paypalcorp.com>
2016-03-01 09:45:11 -0800

    Swing example of Observer pattern

---
+++ src/chapter2b/SwingObserverExample.java
@@ -0,0 +1,53 @@
+package chapter2b;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class SwingObserverExample {
+
+	JFrame frame;
+	
+	public static void main(String[] args) {
+		SwingObserverExample example = new SwingObserverExample();
+		example.go();
+
+	}
+
+	public void go() {
+		frame = new JFrame();
+		JButton button = new JButton("Should I do it?");
+		button.addActionListener(new AngelListener());
+		button.addActionListener(new DevilListener());
+		
+		//set frame properties
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().add(BorderLayout.CENTER, button);
+		frame.setSize(300,300);
+		frame.setVisible(true);
+		
+	}
+	
+	class AngelListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			System.out.println("Don't do it. You might regret it later.");
+			
+		}
+		
+	}
+	
+	class DevilListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			System.out.println("Come on, do it!");		
+		}
+		
+	}
+
+}
